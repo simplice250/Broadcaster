2cc1007fe7d196e95d57d09f36322094bd492111
simplice <simplicenyandekwe@gmail.com>
2019-11-21 23:59:09 +0200

    added index

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname));
+app.use(express.static(__dirname,'../ui'));
 app.use('/', require('./Routes/api/incident'));
 app.use('/', require('./Routes/api/signIn'));
 app.use('/', require('./Routes/api/signUp'));
