5c6f21a3cc2c1729542427c65f90a1ed01b5ba5a
simplice <simplicenyandekwe@gmail.com>
2019-11-25 20:07:55 +0200

    gh-pages

--- UI/javascript/signIn.js
+++ UI/javascript/signIn.js
@@ -1,5 +1,15 @@
 const button=document.querySelector('.btn-signIn');
+const email=document.querySelector('.mail').Value;
+const validation=(email)=>{
+if(email===null){
+    alert("please enter your email");
+    return false;
+}
+ }
+ validation(email);
 button.addEventListener("click",(e)=>{
-    e.preventDefault();
+   e.preventDefault();
     window.location.href = './user/home.html';
+
+    
 })
